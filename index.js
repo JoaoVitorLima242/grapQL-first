5d86602ab754fca079fcff6d4befd8aa57b1b4d1
JoaoVitorLima242 <jlima0500@gmail.com>
2022-09-24 00:31:58 -0300

    graphql types

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 import {gql, ApolloServer} from 'apollo-server'
 
+/* * * * * * * * *
+    * Escalar Types
+    * - Int
+    * - Float
+    * - String
+    * - Boolean
+    * - ID
+*/
+
+const typeDefs = gql`
+    type Query {
+        id: ID
+        nome: String
+        idade: Int
+        salario: Float
+        ativo: Boolean
+    }
+`
+
 const resolvers = {
     Query: {
-        hello() {
-            return 'World'
+        idade() {
+            return 20
+        },
+        salario() {
+            return 10000.99
+        },
+        nome() {
+            return 'Joao'
+        },
+        ativo() {
+            return true
+        },
+        id() {
+            return 1231312312
         }
     }
 }
 
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`
 
 const server = new ApolloServer({
     typeDefs,
